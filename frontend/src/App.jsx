a45a37593ed4646afe9e587c002ab7086d1d6066
Deeksha Nigoti <nigotideeksha@gmqail.com>
2025-10-07 11:12:48 +0530

    Successfully resolved all merge conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-//importing pages
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 import SearchPage from "./pages/SearchPage";
 import UploadPage from "./pages/UploadPage";
 import AboutUs from "./pages/AboutUs";
+import LoginPage from "./pages/LoginPage";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <div data-theme="forest" className="min-h-screen">
       <Routes>
-        {/* defining routes */}
         <Route path="/" element={<HomePage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/upload" element={<UploadPage />} />
         <Route path="/about" element={<AboutUs />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
-    </BrowserRouter>
+    </div>
   );
 };
+
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { Toaster } from 'react-hot-toast';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from './App.jsx';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+      domain="dev-5z1uhgrjd2nen5mw.us.auth0.com"
+      clientId="886zJlf7XANlXhdxbbDff3Am5leDpeEf"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
+    <Toaster />
+  </React.StrictMode>
+);
